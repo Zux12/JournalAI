8f15d2d8363ee0ea22e2374e9fdd7688ee69d0c7
Zux12 <158550944+Zux12@users.noreply.github.com>
2025-09-14 14:52:54 +0800

    Update QnA.jsx

--- src/features/qa/QnA.jsx
+++ src/features/qa/QnA.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { useProjectState } from '../../app/state.js';
+import { useProjectState } from '../../app/state.jsx';
 import { useParams } from 'react-router-dom';
 
 export default function QnA(){
