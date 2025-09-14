0312dfcb71a72ded1062fd5dbb3cc0e74a22055d
Zux12 <158550944+Zux12@users.noreply.github.com>
2025-09-14 14:52:38 +0800

    Update Planner.jsx

--- src/features/planner/Planner.jsx
+++ src/features/planner/Planner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useProjectState } from '../../app/state.js';
+import { useProjectState } from '../../app/state.jsx';
 
 export default function Planner(){
   const { project, setPlanner } = useProjectState();
